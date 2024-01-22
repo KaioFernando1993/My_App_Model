09f9504af519b35b794aad16c93f171b5e968121
Kaio Fernando <104741718+KaioFernando1993@users.noreply.github.com>
2024-01-22 16:10:19 -0300

    rota Detail p/ opinions

--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -5,9 +5,24 @@ sap.ui.define([
     "sap/ui/model/odata/v2/ODataModel",
     "sap/m/MessageToast",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], function(Controller, JSONModel, ODataModel, MessageToast, Filter, FilterOperator) {
+    "sap/ui/model/FilterOperator",
+    "sap/ui/core/library",
+    "sap/m/Dialog",
+    "sap/m/Button",
+    "sap/m/library",
+    "sap/m/Text"
+
+], function(Controller, JSONModel, ODataModel, MessageToast, Filter, FilterOperator, coreLibrary, Dialog, Button, mobileLibrary, Text) {
     "use strict";
+
+    // shortcut for sap.m.ButtonType
+    var ButtonType = mobileLibrary.ButtonType;
+
+    // shortcut for sap.m.DialogType
+    var DialogType = mobileLibrary.DialogType;
+
+    // shortcut for sap.ui.core.ValueState
+    var ValueState = coreLibrary.ValueState;
 
     return Controller.extend("application.controller.Detail", {
         onInit: function() {
@@ -59,6 +74,56 @@ sap.ui.define([
                 }
                 oBindingTable.filter(aFilters);
             }
+        },
+        onRegister: function() {
+            const input1 = this.getView().getModel().getProperty("/bpSap");
+
+            if (input1 === "") {
+                this.messageError();
+            } else {
+                this.messageSuccess();
+            }
+        },
+
+
+        messageSuccess: function() {
+            this.oSuccessMessage = new Dialog({
+                type: DialogType.Message,
+                title: "Success",
+                state: ValueState.Success,
+                content: new Text({ text: "O parecer foi cadastrado e você será redirecionado\n\ à tela de histórico de pareceres." }),
+                beginButton: new Button({
+                    type: ButtonType.Emphasized,
+                    text: "OK",
+                    press: function() {
+                        this.oSuccessMessage.close();
+                        this.onPareceres();
+                    }.bind(this)
+                })
+            });
+            this.oSuccessMessage.open();
+        },
+
+        messageError: function() {
+            this.oErrorMessage = new Dialog({
+                type: DialogType.Message,
+                title: "Error",
+                state: ValueState.Error,
+                content: new Text({ text: "Preencha todos os campos!" }),
+                beginButton: new Button({
+                    type: ButtonType.Emphasized,
+                    text: "OK",
+                    press: function() {
+                        this.oErrorMessage.close();
+                    }.bind(this)
+                })
+            });
+            this.oErrorMessage.open();
+        },
+        // Rota Provisória
+        onPareceres: function() {
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("OpinionsRoute");
         }
     });
 });
